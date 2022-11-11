4a07267db0395c835be9d0e36e71a2dde368a5ed
chennareddipalligayathri <77479095+chennareddipalligayathri@users.noreply.github.com>
2022-11-11 13:55:48 +0530

    Create main.tf
    test the pipeline

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws"{
+region ="us-east-2"
+}
+module "gayathri-server" {
+  source  = "app.terraform.io/Gayathri-Terraform/gayathri-server/aws"
+  version = "1.0.1"
+  hw ="t2.nano"
+servername ="Gayathri-ec2-module-instance"
+# insert required variables here
+}
