56e28c30548fd5bf319242ac24e841cc2d59bbf5
chennareddipalligayathri <77479095+chennareddipalligayathri@users.noreply.github.com>
2022-11-11 14:10:13 +0530

    output update

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@ module "gayathri-server" {
   source  = "app.terraform.io/Gayathri-Terraform/gayathri-server/aws"
   version = "1.0.1"
   hw ="t2.nano"
-servername ="Gayathri-ec2-module-instance"
+  servername ="Gayathri-ec2-module-instance"
 # insert required variables here
+}
+output "ip"{
+value =module.gayathri-server.ip
 }
